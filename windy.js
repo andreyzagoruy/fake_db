5c80b3336833863107c79a782b19c82c129aab86
Andrey Zagoruy <andreyzagoruy@Andreys-Air.lan>
2018-03-28 18:03:58 +0300

    initial windy map usage added

---
+++ windy.js
@@ -0,0 +1,47 @@
+const map = W.maps;
+const queryParameters = {};
+const centersUrl = 'https://my-json-server.typicode.com/andreyzagoruy/fake_db/centers/';
+const centers = [];
+
+function drawCenter(centerId) {
+    fetch(centersUrl + centerId)
+        .then(response => response.json())
+        .then(center => {
+            const centerPosition = L.latLng(parseInt(center.location.lat), parseInt(center.location.lng));
+            const centerMarker = L.marker(centerPosition, {
+                icon: W.map.myMarkers.pulsatingIcon
+            });
+    
+            centers.push(centerMarker);
+            centerMarker.addTo(map);
+            
+            centerMarker.bindPopup(
+                '<h2>' + center.name + '</h2><span class="center-location">' + center.address + '</span><span class="center-rating">, Rating: ' + center.rating + '</span>', {
+                className: 'center-popup'
+            });
+        })
+        .catch(error => {
+            console.error(`Error querying centers: ${error.message}`);
+        });
+}
+
+if (location.search) {
+  location.search.substr(1).split('&').forEach(function(parameter) {
+    const temp = parameter.split('=');
+    const parameterKey = temp[0];
+    const parameterValues = temp[1] && decodeURIComponent(temp[1]);
+    queryParameters[parameterKey] = parameterValues ? parameterValues.split(';') : parameterValues;
+  });
+}
+if (queryParameters['lat'] && queryParameters['long']) {
+    const position = L.latLng(parseInt(queryParameters['lat']), parseInt(queryParameters['long']));
+    map.setView(position, 13);
+} else {
+    map.locate({
+        setView: true
+    });
+}
+
+if (queryParameters['id']) queryParameters['id'].forEach(function(id) {
+    drawCenter(id);
+});
